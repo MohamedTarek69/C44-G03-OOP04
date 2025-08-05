db57525d7cd785ee78f0d29c7ec42e2e3847fbc8
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-05 20:43:13 +0300

    Vedio 2

--- Session 4/Program.cs
+++ Session 4/Program.cs
@@ -2,9 +2,46 @@
 {
     internal class Program
     {
+        static int SumNumbers(int x, int y)
+        {
+            return x + y;
+        }
+        static int SumNumbers(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        static double SumNumbers(double x, double y)
+        {
+            return x + y;
+        }
+        
+        static double SumNumbers(double x, int y)
+        {
+            return x + y;
+        }
+        
+        static double SumNumbers(int x, double y)
+        {
+            return x + y;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //----------------------------------- Vedio 02 -------------------------------------
+            int A = 10 , B = 20 , C = 30 ;
+            //int Result = SumTwoNumbers(A, B);
+            //int Result = SumThreeNumbers(A, B, C);
+            //double Result = SumDoubleTwoNumbers(A, B);
+            //double Result = SumNumbers();
+            //Console.WriteLine(Result);
+
+            //Console.WriteLineInt()
+            //Console.WriteLineString()
+            //Console.WriteLineFloat()
+
+            //Console.WriteLine();
+
+
         }
     }
 }
