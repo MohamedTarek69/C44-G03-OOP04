6e97cb16ced0ffa67ef12faa2b50d51d1bf89585
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-06 13:39:49 +0300

    Q2 Part 01

--- Session 4/Program.cs
+++ Session 4/Program.cs
@@ -17,7 +17,22 @@ namespace Session_4
             //Console.WriteLine("Sum of 3 integers: " + calculator.Add(5, 10, 15));
             //Console.WriteLine("Sum of 2 doubles: " + calculator.Add(5.5, 10.5));
 
-            #endregion 
+            #endregion
+
+            #region Q2) Create a class named Rectangle with the following constructors
+            //A parameterless constructor that sets the width and height to 0.
+            //A constructor that accepts width and height as integers.
+            //A constructor that accepts a single integer and sets both width and height to that value.
+
+            Rectangle rectangle1 = new Rectangle();
+            Console.WriteLine($"Rectangle 1:\n Width: {rectangle1.Width}, Height: {rectangle1.Height}");
+            Rectangle rectangle2 = new Rectangle(5, 10);
+            Console.WriteLine($"Rectangle 2\n Width: {rectangle2.Width}, Height: {rectangle2.Height}");
+            Rectangle rectangle3 = new Rectangle(7);
+            Console.WriteLine($"Rectangle 3\n Width: {rectangle3.Width}, Height: {rectangle3.Height}");
+
+
+            #endregion
 
             #endregion
         }
---
+++ Session 4/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_4
+{
+    internal class Rectangle
+    {
+        #region Properties
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        #endregion
+        #region Constructors
+        public Rectangle()
+        {
+            Width = 0;
+            Height = 0;
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public Rectangle(int size)
+        {
+            Width = size;
+            Height = size;
+        }
+        #endregion
+    }
+}
