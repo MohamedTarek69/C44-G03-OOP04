6b355b7deece335382ce3e5f3108ae498a58c140
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-06 13:42:38 +0300

    Q3 Part 01

---
+++ Session 4/Complex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_4
+{
+    internal class Complex
+    {
+        #region Properties
+        public int Real { get; set; }
+        public int Img { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{Real} + {Img}i";
+        }
+        #endregion
+
+        #region Operators Overloading
+
+        public static Complex operator +(Complex Left, Complex Right)
+        {
+
+            return new Complex
+            {
+                Real = (Left?.Real ?? 0) + (Right?.Real ?? 0),
+                Img = (Left?.Img ?? 0) + (Right?.Img ?? 0),
+            };
+        }
+        public static Complex operator -(Complex Left, Complex Right)
+        {
+            return new Complex
+            {
+                Real = (Left?.Real ?? 0) - (Right?.Real ?? 0),
+                Img = (Left?.Img ?? 0) - (Right?.Img ?? 0),
+            };
+        }
+        #endregion
+    }
+}
--- Session 4/Program.cs
+++ Session 4/Program.cs
@@ -24,13 +24,23 @@ namespace Session_4
             //A constructor that accepts width and height as integers.
             //A constructor that accepts a single integer and sets both width and height to that value.
 
-            Rectangle rectangle1 = new Rectangle();
-            Console.WriteLine($"Rectangle 1:\n Width: {rectangle1.Width}, Height: {rectangle1.Height}");
-            Rectangle rectangle2 = new Rectangle(5, 10);
-            Console.WriteLine($"Rectangle 2\n Width: {rectangle2.Width}, Height: {rectangle2.Height}");
-            Rectangle rectangle3 = new Rectangle(7);
-            Console.WriteLine($"Rectangle 3\n Width: {rectangle3.Width}, Height: {rectangle3.Height}");
+            //Rectangle rectangle1 = new Rectangle();
+            //Console.WriteLine($"Rectangle 1:\n Width: {rectangle1.Width}, Height: {rectangle1.Height}");
+            //Rectangle rectangle2 = new Rectangle(5, 10);
+            //Console.WriteLine($"Rectangle 2\n Width: {rectangle2.Width}, Height: {rectangle2.Height}");
+            //Rectangle rectangle3 = new Rectangle(7);
+            //Console.WriteLine($"Rectangle 3\n Width: {rectangle3.Width}, Height: {rectangle3.Height}");
 
+            #endregion
+
+            #region Q3) Define a class Complex Number that represents a complex number with real and imaginary parts
+            //*Note: Overload the +, - operator to add and subtract two complex numbers.
+            //Complex complex1 = new Complex { Real = 3, Img = 4 };
+            //Complex complex2 = new Complex { Real = 1, Img = 2 };
+            //Complex sum = complex1 + complex2;
+            //Complex difference = complex1 - complex2;
+            //Console.WriteLine($"Sum: {sum}");
+            //Console.WriteLine($"Subtract : {difference}");
 
             #endregion
 
