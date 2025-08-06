20bb3e058e2cfce81ec0d01ebdb0a73386c24b38
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-06 13:36:53 +0300

    Q1 part 01

---
+++ Session 4/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_4
+{
+    internal class Calculator
+    {
+
+        #region Methods
+        public int Add(int a, int b) 
+        { 
+            return a + b; 
+        }
+
+        public int Add(int a, int b, int c) 
+        {
+            return a + b + c; 
+        }
+
+        public double Add(double a, double b) 
+        { 
+            return a + b;
+        } 
+
+        #endregion
+
+    }
+}
--- Session 4/Program.cs
+++ Session 4/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Session_4
+﻿using System;
+
+namespace Session_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01
+
+            #region Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to
+            //Accept two integers and return their sum.
+            //Accept three integers and return their sum.
+            //Accept two doubles and return their sum.
+            //Calculator calculator = new Calculator();
+            //Console.WriteLine("Sum of 2 integers: " + calculator.Add(5, 10));
+            //Console.WriteLine("Sum of 3 integers: " + calculator.Add(5, 10, 15));
+            //Console.WriteLine("Sum of 2 doubles: " + calculator.Add(5.5, 10.5));
+
+            #endregion 
+
+            #endregion
         }
     }
 }
